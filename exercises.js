d8abf5ff6c04578c90fdfdb4a00f4b01c15ffd93
jisu yeo <jisuyeo@jisus-air.myfiosgateway.com>
2021-03-09 23:08:09 -0500

    jisu homework

--- exercises.js
+++ exercises.js
@@ -23,11 +23,11 @@ const album1 = {
 
 // 1. Retrieve the string "Sire" from album1, and save it as 
 //    albumOneLabel.
-
+let albumOneLabel = album1.albumDetails.label
 
 
 // 2. Change the title of album1 from "Talking Heads" to "Talking Heads: 77"
-
+album1['title'] = "Talking Heads: 77"
 
 
 const album2 = {
@@ -57,7 +57,7 @@ album3.albumDetails.formats.push(album2.albumDetails.formats[0]);
 
 // 4. Change the release date of album3 from a string into a Date object
 // Look ahead to album4 for a clue!
-
+album3['released'] = new Date('August 3, 1979')
 
 const album4 = {
   title: "Remain in Light",
@@ -70,7 +70,7 @@ const album4 = {
 
 
 // 5. Add the label "Sire" to album4's details
-
+album4['albumDetails']['label'] = 'Sire'
 
 
 const album5 = {
@@ -82,7 +82,7 @@ const album5 = {
 };
 
 // 6. Add a 'formats' array to album 5 and add "CD", "Cassette", and "LP"
-
+album5['albumDetails']['formats'] = ['CD', 'cassette', 'LP']
 const album6 = {
   title: "Little Creatures",
   albumDetails: {
@@ -94,7 +94,7 @@ const album6 = {
 
 // 7. Make the label "emi" in album6 all uppercase
 // google how to make a string uppercase in js!
-
+album6.albumDetails.labels[1] = album6.albumDetails.labels[1].toUpperCase()
 
 
 const album7 = {
@@ -109,7 +109,7 @@ const album7 = {
 // 8. Convert album7's 'labels' property from the string value
 //    "Sire, EMI" into the array: ["Sire", "EMI"]
 // google js array split!
-
+album7['albumDetails']['labels'] = album7['albumDetails']['labels'].split(', ')
 
 
 const album8 = {
@@ -137,23 +137,24 @@ const talkingHeadsAlbums = [
 /////////////////////////////////////////////////////
 
 // 1. Create an object literal called `band`.
-
+const band = {}
 
 // 2. Give it the property `name` and set it to "Talking Heads"
- 
+ band['name'] = "Talking Heads"
 
 // 3. Give it the property `members` and set it to an array with a single
 //    string, "David Byrne", in it.
-
+band['members'] = ["David Byrne"]
 
 // 4. Give it the property `albums` and set it to the array stored in the
 //    variable talkingHeadsAlbums
-
+band['albums'] = talkingHeadsAlbums
 
 // 5. Add "Tiny Weymouth", "Chris Franz" and "Jerry Harrison" to the members
 //    array.
-
-
+band['members'].push('Tiny Weymouth')
+band['members'].push('Chris Franz')
+band['members'].push('Jerry Harrison')
 
 ////////////////////////////////////////////////
 // Part 4: Conditional Logic
@@ -163,7 +164,11 @@ const talkingHeadsAlbums = [
 //    if the Talking Heads have 6 albums or more. Otherwise, console.log
 //    "Talking heads didn't have much output." Use the array of albums
 //    talkingHeadsAlbums above.
-let opinion;
+if (talkingHeadsAlbums.length >= 6){
+  opinion = "Talking Heads were a prolific band"
+} else {
+  opinion ="Talking heads didn't have much output"
+}
 
 
 
@@ -172,7 +177,12 @@ let opinion;
 //    talkingHeadsAlbums is odd or even, and then console.log
 //    "The number X is odd" or "The number X is even" with X being
 //    the number of albums.
-
+let numAlbums = talkingHeadsAlbums.length
+if(numAlbums % 2 === 0) {
+    console.log("The number", numAlbums, "is even")
+} else {
+    console.log("The number", numAlbums, "is odd")
+}
 
 
 // 3. Write conditionals to check if the number of albums in
