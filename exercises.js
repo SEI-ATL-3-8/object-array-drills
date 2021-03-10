059ca57f795341273d1babeae9a323f38594cda2
rz4life <rzalagbada@Abdulrazaqs-Air.cable.rcn.com>
2021-03-09 23:45:46 -0600

    Rz home work

--- exercises.js
+++ exercises.js
@@ -23,10 +23,12 @@ const album1 = {
 
 // 1. Retrieve the string "Sire" from album1, and save it as 
 //    albumOneLabel.
+let albumOneLabel = album1.albumDetails.label
 
 
 
 // 2. Change the title of album1 from "Talking Heads" to "Talking Heads: 77"
+ album1.title = "Talking Heads: 77"
 
 
 
@@ -52,12 +54,16 @@ const album3 = {
 //    album3's formats
 // Check out the Array.push method!
 
+//album3.formats = album2.formats[0].push
+
+album3.albumDetails.formats.push(album2.albumDetails.formats[0]);
+
 
 
 
 // 4. Change the release date of album3 from a string into a Date object
 // Look ahead to album4 for a clue!
-
+album3.released = new Date ("August 3, 1979");
 
 const album4 = {
   title: "Remain in Light",
@@ -71,7 +77,7 @@ const album4 = {
 
 // 5. Add the label "Sire" to album4's details
 
-
+album4.albumDetails.label = "Sire"
 
 const album5 = {
   title: "Speaking in Tongues",
@@ -82,6 +88,9 @@ const album5 = {
 };
 
 // 6. Add a 'formats' array to album 5 and add "CD", "Cassette", and "LP"
+//album5.albumDetails.formats = ["CD", "Cassette", "LP"];
+album5.albumDetails.formats = ["CD", "cassette", "LP"];
+
 
 const album6 = {
   title: "Little Creatures",
@@ -94,7 +103,7 @@ const album6 = {
 
 // 7. Make the label "emi" in album6 all uppercase
 // google how to make a string uppercase in js!
-
+album6.albumDetails.labels[1] = album6.albumDetails.labels[1].toUpperCase();
 
 
 const album7 = {
@@ -109,7 +118,7 @@ const album7 = {
 // 8. Convert album7's 'labels' property from the string value
 //    "Sire, EMI" into the array: ["Sire", "EMI"]
 // google js array split!
-
+album7.albumDetails.labels = ["Sire", "EMI"]
 
 
 const album8 = {
@@ -137,22 +146,28 @@ const talkingHeadsAlbums = [
 /////////////////////////////////////////////////////
 
 // 1. Create an object literal called `band`.
-
+const band = {};
 
 // 2. Give it the property `name` and set it to "Talking Heads"
+
+band.name = "Talking Heads";
  
 
 // 3. Give it the property `members` and set it to an array with a single
 //    string, "David Byrne", in it.
+band.members = [ "David Byrne"];
 
 
 // 4. Give it the property `albums` and set it to the array stored in the
 //    variable talkingHeadsAlbums
+band.albums = talkingHeadsAlbums;
 
 
 // 5. Add "Tiny Weymouth", "Chris Franz" and "Jerry Harrison" to the members
 //    array.
-
+band.members.push("Tiny Weymouth") 
+band.members.push("Chris Franz")
+band.members.push("Jerry Harrison") 
 
 
 ////////////////////////////////////////////////
@@ -164,7 +179,11 @@ const talkingHeadsAlbums = [
 //    "Talking heads didn't have much output." Use the array of albums
 //    talkingHeadsAlbums above.
 let opinion;
-
+if (talkingHeadsAlbums.length >= 6) {
+  opinion = "Talking Heads were a prolific band";
+} else {
+  opinion = "Talking heads didnt have much output";
+}
 
 
                                                                                                                                                                                                 
@@ -172,8 +191,13 @@ let opinion;
 //    talkingHeadsAlbums is odd or even, and then console.log
 //    "The number X is odd" or "The number X is even" with X being
 //    the number of albums.
-
-
+var result;
+var numAlbums = talkingHeadsAlbums.length;
+if(numAlbums % 2 === 0) {
+  result = ("even")
+} else {
+  result = ("odd")
+}
 
 // 3. Write conditionals to check if the number of albums in
 //    talkingHeadsAlbums is divisible by either 2 or 3, and then
