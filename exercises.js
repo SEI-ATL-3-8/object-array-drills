553a3ebf6fd1cc1a7d080bf066d30226faf32598
Eric <echen742@gmail.com>
2021-03-09 23:20:58 -0500

    homework

--- exercises.js
+++ exercises.js
@@ -24,11 +24,11 @@ const album1 = {
 // 1. Retrieve the string "Sire" from album1, and save it as 
 //    albumOneLabel.
 
-
+let alubumOneLable =album1.albumDetails.label;
 
 // 2. Change the title of album1 from "Talking Heads" to "Talking Heads: 77"
 
-
+album1.title ="Talking Heads: 77";
 
 const album2 = {
   title: "More Songs About Buildings and Food",
@@ -53,11 +53,12 @@ const album3 = {
 // Check out the Array.push method!
 
 album3.albumDetails.formats.push(album2.albumDetails.formats[0]);
-
+album3.albumDetials.formats.push(album2.albumDetials.formats[0]);
 
 // 4. Change the release date of album3 from a string into a Date object
 // Look ahead to album4 for a clue!
 
+album3.albumDetails.released= new Date("August 3, 1979");
 
 const album4 = {
   title: "Remain in Light",
@@ -71,7 +72,7 @@ const album4 = {
 
 // 5. Add the label "Sire" to album4's details
 
-
+album4.albumDetails.label="Sire";
 
 const album5 = {
   title: "Speaking in Tongues",
@@ -82,6 +83,8 @@ const album5 = {
 };
 
 // 6. Add a 'formats' array to album 5 and add "CD", "Cassette", and "LP"
+
+album5['formats']={'CD', 'Cassette', 'LP'};
 
 const album6 = {
   title: "Little Creatures",
@@ -95,7 +98,7 @@ const album6 = {
 // 7. Make the label "emi" in album6 all uppercase
 // google how to make a string uppercase in js!
 
-
+album6.albumDetails.labels[1]=album6.albumDetails.labels[1].toUpperCase;
 
 const album7 = {
   title: "True Stories",
@@ -109,6 +112,7 @@ const album7 = {
 // 8. Convert album7's 'labels' property from the string value
 //    "Sire, EMI" into the array: ["Sire", "EMI"]
 // google js array split!
+album7.labels=album7.lables.split(", ");
 
 
 
@@ -137,23 +141,32 @@ const talkingHeadsAlbums = [
 /////////////////////////////////////////////////////
 
 // 1. Create an object literal called `band`.
-
+const band={}
 
 // 2. Give it the property `name` and set it to "Talking Heads"
  
-
+const band={name: "Talking Heads"};
 // 3. Give it the property `members` and set it to an array with a single
 //    string, "David Byrne", in it.
-
+const band={
+  name: "Talking Heads",
+  members: ['David Byrne']
+};
 
 // 4. Give it the property `albums` and set it to the array stored in the
 //    variable talkingHeadsAlbums
-
+const band={
+  name: "Talking Heads",
+  members: ['David Byrne'],
+  albums: talkingHeadsAlbums
+};
 
 // 5. Add "Tiny Weymouth", "Chris Franz" and "Jerry Harrison" to the members
 //    array.
 
-
+band.members.push("Tiny Weymouth");
+band.members.push("Chris Franz");
+band.members.push("Jerry Harrison");
 
 ////////////////////////////////////////////////
 // Part 4: Conditional Logic
@@ -165,7 +178,10 @@ const talkingHeadsAlbums = [
 //    talkingHeadsAlbums above.
 let opinion;
 
-
+if(talkingHeadsAlbums.length>=6)
+console.log('Talking Heads were a prolific band');
+else
+console.log("Talking heads didn't have much output.")
 
                                                                                                                                                                                                 
 // 2. Write a conditional to check if the number of albums in
@@ -173,7 +189,10 @@ let opinion;
 //    "The number X is odd" or "The number X is even" with X being
 //    the number of albums.
 
-
+if(talkingHeadsAlbums.length%2===0)
+console.log("The number "+ talkingHeadsAlbums.length+" is even")
+else
+console.log("The number " +talkingHeadsAlbums.length+ " is odd")
 
 // 3. Write conditionals to check if the number of albums in
 //    talkingHeadsAlbums is divisible by either 2 or 3, and then
@@ -184,8 +203,15 @@ let opinion;
 //    - "The number Y is not divisible by 2 or 3",
 //
 //    with Y being the number of albums.
-
-
+let x =talkingHeadsAlbums.length;
+if(x%2===0 &&x%3===0)
+console.log('The number '+x+' is divisible by 2 and 3')
+else if(x%2===0)
+console.log('The number '+x+' is divisible by 2')
+else if(x%3===0)
+console.log('The number '+x+' is divisible by 3')
+else
+console.log('The number '+x+ ' is not divisible by 2 or 3')
 
 
 // 4. Check your logic above against the numbers: 0, 1, 2, 6, 7, and 9.
