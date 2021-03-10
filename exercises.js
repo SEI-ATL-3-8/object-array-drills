34bf3d919c0ac5ba1e4e4043e81e801db343e167
Derek Ensign <densign@Dereks-Air.attlocal.net>
2021-03-09 22:45:46 -0600

    adding an updated exercise.js file

--- exercises.js
+++ exercises.js
@@ -24,11 +24,12 @@ const album1 = {
 // 1. Retrieve the string "Sire" from album1, and save it as 
 //    albumOneLabel.
 
+let albumOneLabel = album1['albumDetails']['label']
 
 
 // 2. Change the title of album1 from "Talking Heads" to "Talking Heads: 77"
 
-
+album1['title'] = 'Talking Heads: 77'
 
 const album2 = {
   title: "More Songs About Buildings and Food",
@@ -52,12 +53,17 @@ const album3 = {
 //    album3's formats
 // Check out the Array.push method!
 
+album3.albumDetails.formats.push(album2.albumDetails.formats[0])
 
+console.log(album3.albumDetails.formats)
 
 
 // 4. Change the release date of album3 from a string into a Date object
 // Look ahead to album4 for a clue!
 
+album3.albumDetails.released = new Date(album3.albumDetails.released) 
+
+console.log(album3.albumDetails.released)
 
 const album4 = {
   title: "Remain in Light",
@@ -71,6 +77,8 @@ const album4 = {
 
 // 5. Add the label "Sire" to album4's details
 
+album4.albumDetails.label = 'Sire'
+
 
 
 const album5 = {
@@ -82,6 +90,8 @@ const album5 = {
 };
 
 // 6. Add a 'formats' array to album 5 and add "CD", "Cassette", and "LP"
+
+album5.albumDetails.formats = ['CD', 'cassette', 'LP']
 
 const album6 = {
   title: "Little Creatures",
@@ -95,6 +105,7 @@ const album6 = {
 // 7. Make the label "emi" in album6 all uppercase
 // google how to make a string uppercase in js!
 
+album6.albumDetails.labels[1] = album6.albumDetails.labels[1].toUpperCase();
 
 
 const album7 = {
@@ -109,6 +120,8 @@ const album7 = {
 // 8. Convert album7's 'labels' property from the string value
 //    "Sire, EMI" into the array: ["Sire", "EMI"]
 // google js array split!
+
+album7.albumDetails.labels = album7.albumDetails.labels.split(", ");
 
 
 
@@ -138,20 +151,31 @@ const talkingHeadsAlbums = [
 
 // 1. Create an object literal called `band`.
 
+const band = new Object();
+
 
 // 2. Give it the property `name` and set it to "Talking Heads"
+
+band.name = 'Talking Heads'
+
  
 
 // 3. Give it the property `members` and set it to an array with a single
 //    string, "David Byrne", in it.
 
+band.members = ['David Byrne']
 
 // 4. Give it the property `albums` and set it to the array stored in the
 //    variable talkingHeadsAlbums
 
+band.albums = talkingHeadsAlbums
 
 // 5. Add "Tiny Weymouth", "Chris Franz" and "Jerry Harrison" to the members
 //    array.
+
+band.members.push('Tiny Weymouth')
+band.members.push('Chris Franz')
+band.members.push('Jerry Harrison')
 
 
 
@@ -165,15 +189,24 @@ const talkingHeadsAlbums = [
 //    talkingHeadsAlbums above.
 let opinion;
 
+if (talkingHeadsAlbums.length >= 6) {
+  opinion = 'Talking Heads were a prolific band';}
+else { opinion = 'Talking heads didn\'t have much output';
+}
 
+console.log(opinion)
 
-                                                                                                                                                                                                
 // 2. Write a conditional to check if the number of albums in
 //    talkingHeadsAlbums is odd or even, and then console.log
 //    "The number X is odd" or "The number X is even" with X being
 //    the number of albums.
 
+var numAlbums = talkingHeadsAlbums.length
 
+if (numAlbums % 2 == 0) {
+     console.log('The number ' + numAlbums + ' is even');}
+else { console.log('The number ' + numAlbums + ' is odd');
+}
 
 // 3. Write conditionals to check if the number of albums in
 //    talkingHeadsAlbums is divisible by either 2 or 3, and then
@@ -184,6 +217,18 @@ let opinion;
 //    - "The number Y is not divisible by 2 or 3",
 //
 //    with Y being the number of albums.
+
+
+if(numAlbums === 0) {
+  console.log("The number 0 is not divisible by 2 or 3 ");}
+else if ( numAlbums % 2 == 0 && numAlbums % 3 == 0){
+  console.log('The number ' + numAlbums + ' is divisible by 2 and 3');}
+else if (numAlbums % 2 == 0 ){ 
+  console.log('The number ' + numAlbums + ' is divisible by 2');}
+else if (numAlbums % 3 == 0 ){
+  console.log('The number ' + numAlbums + ' is divisible by 3');}
+else {
+  console.log('The number ' + numAlbums + ' is not divisible by 2 or 3');}
 
 
 
